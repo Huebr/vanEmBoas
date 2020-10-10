0d0a2ac66c2964158667c1d89d898c14359d8ac8
HueBr <pedro.jorge@aluno.uece.br>
2020-10-10 16:11:58 -0300

    Prototipo Completo

---
+++ vamEmBoas/Hashing.h
@@ -0,0 +1,177 @@
+// SimpleTableHashing.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+#include <chrono>
+#include <stdint.h>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <cstdio>
+#pragma warning(disable:4996)//disable some vs safety warnings
+
+using namespace std;
+
+template <class elem>
+struct item { 
+	int64_t data = 0;
+	elem *p = nullptr;
+	int8_t  bit = 0;
+};
+
+template <class element,class tipo>
+struct hashtable{
+	int64_t T[8][256];
+	int64_t n_elems = 0;
+	int64_t n_removed = 0;
+	vector<element> *ht;
+	//create table to be used by simple hashing
+	hashtable() {
+		ht = new vector<element>(4);
+		ht->resize(4);
+		initialize_table();
+	}
+	hashtable(int size) {
+		ht = new vector<element>(size);
+		ht->resize(size);
+		initialize_table();
+	}
+	~hashtable() {
+		if (ht != nullptr) {
+			for (int i = 0; i < ht->capacity(); i++)
+			{
+				if ((*ht)[i].p != nullptr) {
+					delete (*ht)[i].p;
+					(*ht)[i].p = nullptr;
+				}
+			}
+			delete ht;
+			ht = nullptr;
+		}
+	}
+
+	void initialize_table() {
+		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+		std::mt19937 generator(seed);
+
+		for (int i = 0; i < 8; ++i) {
+			for (int j = 0; j < 256; ++j) {
+				T[i][j] = generator();
+			}
+		}
+
+	}
+
+	// simple table hashing implementation
+	int64_t SimpleTableHashing(int64_t input)
+	{
+		int64_t i;
+		int64_t h = 0;
+		int8_t c;
+		for (i = 0; i < 8; i++) {
+			c = input;
+			h ^= T[i][c];
+			input = input >> 8;
+		}
+		return h;
+	}
+
+
+	void dobrar() {
+		int64_t m = ht->capacity();
+		vector<element> *temp = ht;
+		ht = new vector<element>(m * 2);
+		n_removed = 0;
+		for (int i = 0; i < m; ++i) {
+			if ((*temp)[i].bit == 1)incluir((*temp)[i].data, (*temp)[i].p,false);
+		}
+		delete temp;
+	}
+
+	void dividir() {
+		int64_t m = ht->capacity();
+		vector<element> *temp = ht;
+		ht = new vector<element>(m / 2);
+		n_removed = 0;
+		for (int i = 0; i < m; ++i) {
+			if ((*temp)[i].bit == 1)incluir((*temp)[i].data, (*temp)[i].p,false);
+		}
+		delete temp;
+	}
+
+
+	
+	void incluir(int64_t data,tipo *ponteiro,bool debug) {
+		int64_t hash = SimpleTableHashing(data);
+		int64_t m = ht->capacity();
+		for (int i = 0; i < m; ++i) {
+			int64_t idx = (hash + i) % m;
+			if ((*ht)[idx].bit != 1) {
+				if ((*ht)[idx].bit == 3) n_removed--;
+				(*ht)[idx].data = data;
+				(*ht)[idx].p = ponteiro;
+				(*ht)[idx].bit = 1;
+				break;
+			}
+		}
+		//use debug variable to verify if function is external or internal
+		if (debug) n_elems++;
+		if (n_elems > (m/2)&&debug) {
+			dobrar();
+		}
+
+	}
+
+	tipo* buscar(int64_t data) {
+		int64_t hash = SimpleTableHashing(data);
+		int64_t m = ht->capacity();
+		for (int i = 0; i < m; ++i) {
+			int64_t idx = (hash + i) % m;
+			if (data == (*ht)[idx].data && (*ht)[idx].bit == 1) {
+				return (*ht)[idx].p;
+			}
+			else if ((*ht)[idx].bit == 0) break;
+		}
+		return nullptr;
+	}
+	void limpar() {
+		int64_t m = ht->capacity();
+		vector<element> *temp = ht;
+		ht = new vector<element>(m);
+		for (int i = 0; i < m; ++i) {
+			if ((*temp)[i].bit == 1)incluir((*temp)[i].data, (*temp)[i].p, false);
+		}
+		delete temp;
+	}
+
+	void remover(int64_t data) {
+		int64_t hash = SimpleTableHashing(data);
+		int64_t m = ht->capacity();
+		bool flag=false;
+		bool cleared = false;
+
+		for (int i = 0; i < m; ++i) {
+			int64_t idx = (hash + i) % m;
+			if ((*ht)[idx].data == data && (*ht)[idx].bit == 1) {
+				(*ht)[idx].bit = 3;
+				delete (*ht)[idx].p;
+				(*ht)[idx].p = nullptr;
+				n_removed++;
+				cleared = true;
+				break;
+			}
+		}
+
+		if (cleared) {
+			n_elems--;
+			if (n_elems < (m / 4)) {
+				dividir();
+			}
+			else if (n_removed > m / 4) limpar();
+		}
+		else cout << hash % m << " " << -1 << endl;
+	}
+
+};
+
+
+
+
--- vamEmBoas/vamEmBoas.cpp
+++ vamEmBoas/vamEmBoas.cpp
@@ -1,21 +1,208 @@
 // vamEmBoas.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
 //
-
-#include "pch.h"
+//
+#include <chrono>
+#include <stdint.h>
 #include <iostream>
+#include <random>
+#include <vector>
+#include <cstdio>
+#include "Hashing.h"
+#pragma warning(disable:4996)//disable some vs safety warnings
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+using namespace std;
+
+int64_t high(int64_t x) {
+	return x >> 32;
+
 }
 
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+int64_t lower(int64_t x) {
+	return x & ((1 << 32) - 1);
+}
 
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
+int64_t join(int64_t c, int64_t i) {
+	return (c << 32) | i;
+}
+
+
+
+struct VEB {
+	
+	int64_t* min = nullptr;
+	int64_t max;
+	VEB* resumo = nullptr;
+	hashtable<item<VEB>,VEB> cluster;
+
+	//create table to be used by simple hashing
+	VEB() {
+
+	}
+	~VEB() {
+		delete min;
+		delete resumo;
+		cluster.~hashtable();
+	}
+
+	void incluir(int64_t x) {
+		if (min == nullptr) {
+			min = new int64_t(x);
+			max = x;
+		}
+		else {
+			if (x < *(min)) {
+				int temp = x;
+				x = *(min);
+				*(min) = temp;
+			}
+
+			if (cluster.buscar(high(x)) == nullptr) {
+				cluster.incluir(high(x), new VEB(),true);
+			}
+			if (cluster.buscar(high(x))->min == nullptr) {
+				if (resumo == nullptr) resumo = new VEB();
+				(*resumo).incluir(high(x));
+			}
+			if (x > max) {
+				max = x;
+			}
+			cluster.buscar(high(x))->incluir(lower(x));
+		}
+
+	}
+
+	int64_t* predecessor(int64_t x) {
+		int64_t* out;
+		if (min == nullptr) return nullptr;
+		else if (x > max) {
+			out = new int64_t(max);
+			return out;
+		}
+		else if (x < *(min)) return nullptr;
+		else if (cluster.buscar(high(x))!=nullptr&&lower(x) > *(cluster.buscar(high(x))->min)) {
+			int64_t p = join(high(x), *(cluster.buscar(high(x))->predecessor(lower(x))));
+			out = new int64_t(p);
+			return out;
+		}
+		int64_t c1 = *(resumo->predecessor(high(x)));
+		out = new int64_t(join(c1, cluster.buscar(c1)->max));
+		return out;
+	}
+
+	int64_t* sucessor(int64_t x) {
+		int64_t* out;
+		if (min == nullptr) return nullptr;
+		else if (x > max) return nullptr;
+		else if (x < *(min)) return min;
+		else if (cluster.buscar(high(x)) != nullptr&&lower(x) < cluster.buscar(high(x))->max) {
+			int64_t p = join(high(x),*(cluster.buscar(high(x))->sucessor(lower(x))));
+			out = new int64_t(p);
+			return out;
+		}
+		int64_t c1 = *(resumo->sucessor(high(x)));
+		out = new int64_t(join(c1, *(cluster.buscar(c1)->min)));
+		return out;
+	}
+
+
+	void remover(int64_t x) {
+		if (min == nullptr) return;
+		if (x == *(min)) {
+			if (resumo == nullptr) {
+				delete min;
+				min = nullptr;
+				return;
+			}
+			int64_t c = *(resumo->min);
+			*(min) = join(c,*(cluster.buscar(c)->min));
+			x = *(min);
+		}
+		cluster.buscar(high(x))->remover(lower(x));
+		if (cluster.buscar(high(x))->min == nullptr) {
+			cluster.remover(high(x));
+			resumo->remover(high(x));
+		}
+		if (resumo->min == nullptr) {
+			max = *(min);
+			delete resumo;
+			resumo = nullptr;
+		}
+		else {
+			int64_t c1 = resumo->max;
+			max = join(c1, cluster.buscar(c1)->max);
+		}
+	}
+
+};
+
+
+
+
+int main(int argc, char **argv)
+{
+	if (argc != 3) {
+		cout << "Arquivos não foram passados como argumento" << endl;
+		cout << "usage: program input_name.txt output_name.txt" << endl;
+		cout << "obs: crie o arquivo de saida antes de chamar o programa." << endl;
+		return -1;
+	}
+
+	FILE *fp;
+	fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+		cout << "erro abrindo arquivo de entrada.";
+		return -1;
+	}
+	if (freopen(argv[2], "w+", stdout) == NULL) {
+		cout << "erro abrindo arquivo de saida.";
+		return -1;
+	}
+
+	char op[3];
+	VEB veb;
+	int data;
+	char trash;
+	int64_t *result;
+
+
+	if (fscanf(fp, "%c%c%c:%d", &op[0], &op[1], &op[2], &data) == 4) {
+		do {
+			switch (op[0])
+			{
+			case 'I':
+				cout << "INC:" << data << endl;
+				veb.incluir(data);
+				break;
+			case 'R':
+				cout << "REM:" << data << endl;
+				veb.remover(data);
+				break;
+			case 'S':
+				cout << "SUC:" << data << endl;
+				result = veb.sucessor(data);
+				if (result != nullptr) {
+					cout << *(result) << endl;
+					delete result;
+					result = nullptr;
+				}
+				else cout << "NULO"<< endl;
+				break;
+			case 'P':
+				cout << "PRE:" << data << endl;
+				result = veb.predecessor(data);
+				if (result != nullptr) {
+					cout << *(result) << endl;
+					delete result;
+					result = nullptr;
+				}
+				else cout << "NULO" << endl;
+				break;
+			default:
+				cout << "codigo de operacao invalido" << endl;
+				break;
+			}
+			cout << endl;
+		} while (fscanf(fp, "%c%c%c%c:%d", &trash, &op[0], &op[1], &op[2], &data) == 5);
+	}
+
+}
